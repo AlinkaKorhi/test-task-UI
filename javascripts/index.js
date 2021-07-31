f81d71114fa799f355b63af5fc1e2b9a77bfc973
Alina Karkhavaya <korhi1536@gmail.com>
2021-07-31 18:42:38 +0300

    add create new comment

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,10 +1,25 @@
 async function GetComments() {
     $.ajax({
         url: `${window.serverApiUrl}/comments/`,
+        method: "GET", 
         success: function(response) {
             result = response;
             // fill list of comments
             createListOfComments(result);
+        }
+    });
+}
+
+async function CreateComment(comment) {
+    $.ajax({        
+        url: `${window.serverApiUrl}/comments/`,
+        method: "POST", 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(comment),
+        success: function() {   
+            GetComments();         
         }
     });
 }
@@ -16,8 +31,13 @@ function createListOfComments(comments){
         if (element.parentId == 0){
             const li = document.createElement('li');
             li.setAttribute('id', 'li_'+element.id);
-            li.innerHTML = element.text + ': '+ element.userName + ', '+ element.date;
-            ul.appendChild(li);
+
+            const liCheck = document.getElementById('li_' + element.id);
+            if (liCheck == null){
+                li.innerHTML = element.text + ': '+ element.userName + ', '+ element.date;
+                ul.appendChild(li);
+            }
+            
         }
         else
         {
@@ -25,16 +45,60 @@ function createListOfComments(comments){
             
             const parentUl = document.createElement('ul');
             parentUl.setAttribute('id', 'ul_'+element.parentId);
-            const li = document.createElement('li');
-            li.setAttribute('id', 'li_'+element.id);
-            li.innerHTML = element.text + ': '+ element.userName + ', '+ element.date;
-            parentLi.appendChild(parentUl);
-            parentUl.appendChild(li);
+
+            const ulCheck = document.getElementById('ul_' + element.parentId);
+            if (ulCheck == null){
+                const li = document.createElement('li');
+                li.setAttribute('id', 'li_'+element.id);
+                const liCheck = document.getElementById('li_' + element.id);
+                if (liCheck == null){
+                    li.innerHTML = element.text + ': '+ element.userName + ', '+ element.date;
+                    parentLi.appendChild(parentUl);
+                    parentUl.appendChild(li);
+                }              
+            }
+
         }
     });
 
 }
 
+function validation(comment) {
+
+    if (comment.date == "") {
+        return false;
+    }
+    if (comment.userName == "") {
+        return false;
+    }
+    if (comment.text == "") {
+        return false;
+    }
+    return true;
+}
+
 (async function () {
+    document.getElementById("send_btn").addEventListener("click", function() {
+        const userName = document.getElementById("user_name_input").value;
+        const text = document.getElementById("text_input").value;
+
+        let comment = {};
+        comment.id = 0;
+        comment.date = new Date();
+        comment.userName = userName;
+        comment.text = text;
+
+        const isValid = validation(comment);
+
+        if (!isValid) {
+            alert("Validation error. All fields must be filled.");
+            return;
+        }
+
+        CreateComment(comment);
+    });
+
+    
     await GetComments();
+
 })()
